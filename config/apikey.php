6f6e415870663ae353b024c6066855f1ddbc3c50
deel18 <deel18.github@gmail.com>
2020-01-18 14:56:37 +0000

    added travis

--- config/apikey.php
+++ config/apikey.php
@@ -5,5 +5,3 @@ return [
     "darksky" => "5251c5ae1657dbaec0ec8d90b14620c3",
     "openstreetmap" => "pk.eyJ1Ijoib25seWhlcmVmb3JtYXBzIiwiYSI6ImNrNGFpYjE4cTA0NG4zbm9na2h2OGRpajMifQ.OIVwPYodrfKazUvT1TSeYA"
 ];
-
-?>
