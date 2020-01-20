1077f84c4f75088b40e8fb4fb67507d67d7cd03e
deel18 <deel18.github@gmail.com>
2020-01-20 14:23:47 +0000

    Cleaned up files

--- test/Controller/WeatherJsonControllerTest.php
+++ test/Controller/WeatherJsonControllerTest.php
@@ -64,5 +64,17 @@ class WeatherJsonControllerTest extends TestCase
         $this->assertIsArray($res);
 
         $this->assertArrayHasKey("valid", $res[0]);
+
+        //Coordinates test
+        $request->setGet("latitude", "56.160820");
+        $request->setGet("longitude", "15.586710");
+        $request->setGet("verify", "Verify");
+
+        $res = $controller->weatherjsonActionGet();
+        $this->assertIsArray($res);
+
+        $this->assertArrayHasKey("valid", $res[0]);
+
+
     }
 }
